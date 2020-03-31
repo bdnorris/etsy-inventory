8eac71574440b787854dab97fcaa872e228b140b
Brian <bdnorris@gmail.com>
2020-03-30 20:50:44 -0500

    removing vue app styles

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,6 @@ import Airtable from 'airtable'
 
 const base = new Airtable({apiKey: 'keyuT2yD8poDXvOPF'}).base('apptGwYXiNB8Jn1wg');
 
-
 export default {
   components: {
     // FormCollectionAT
@@ -58,27 +57,5 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
